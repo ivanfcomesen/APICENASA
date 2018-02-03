90ce33f296d90f8b14599cf424e3b7401a4d2f09
usuario_pc <usuario_pc@usuario>
2018-02-03 17:06:34 -0600

    AdminLTE

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>@yield('title')</title>
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+         <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{asset('font-awesome/css/font-awesome.min.css')}}" >
         @yield('stylesheets')
     </head>
